652b892249649b2116f8651c28af2fa907f5c532
Sanchit Sharma <sanchitsharma1@gmail.com>
2016-04-03 12:03:53 +0100

    Fixed the bug from previous commit!

--- systems/PhysicsSystem.py
+++ systems/PhysicsSystem.py
@@ -4,23 +4,21 @@ class PhysicsSystem(System):
 
     def __init__(self):
         super(PhysicsSystem, self).__init__();
-        self.requirements = ('PositionComponent', 'AccelerationComponent', 'VelocityComponent')
+        self.requirements = ('Position', 'Acceleration', 'Velocity')
 
     def process(self, entities):
         friction = 0
         dt = (1.0 / 60.0)
         for entity in entities:
-            positionComponent = entity.getComponent('PositionComponent')
-            velocityComponent = entity.getComponent('VelocityComponent')
-            accelerationComponent = entity.getComponent('AccelerationComponent')
+            positionComponent = entity.getComponent('Position')
+            velocityComponent = entity.getComponent('Velocity')
+            accelerationComponent = entity.getComponent('Acceleration')
             currentPosition = positionComponent.value
             currentVelocity = velocityComponent.value
             currentAcceleration = accelerationComponent.value
 
-            '''
-                Velocity Verlot Method:
-                http://gamedev.stackexchange.com/questions/15708/how-can-i-implement-gravity
-            '''
+            #   Velocity Verlot Method:
+            #s   http://gamedev.stackexchange.com/questions/15708/how-can-i-implement-gravity
 
             newPosition = currentPosition + dt * (currentVelocity + dt * currentAcceleration / 2)
             newVelocity = currentVelocity + dt * currentAcceleration
