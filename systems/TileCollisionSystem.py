93a579b86ec36dab25f9b34385b6ce77fc7d14cf
Aviv Beeri <ab4211@ic.ac.uk>
2016-04-08 08:17:51 +0100

    Last collision bug for a while, i hope

--- systems/TileCollisionSystem.py
+++ systems/TileCollisionSystem.py
@@ -81,3 +81,4 @@ class TileCollisionSystem(System):
                     event = pygame.event.Event(pygame.USEREVENT, data)
                     collidable.handle(event)
                     other.getComponent('Collidable').handle(event)
+                    collisions.add(collision)
