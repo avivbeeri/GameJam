dd42b62f98ee6ee52f7e19a0ff5be5bd04ebc2ab
Sanchit Sharma <sanchitsharma1@gmail.com>
2016-04-17 13:53:38 +0100

    More progress on sound system. It now imports :p

--- systems/SoundSystem.py
+++ systems/SoundSystem.py
@@ -1,17 +1,32 @@
 from ecs import System
 import pygame
 
+pygame.mixer.init()
+
 class SoundSystem(System):
 
     def __init__(self):
         super(SoundSystem, self).__init__();
         self.requirements = ('EventHandler',)
+        world.on([pygame.USEREVENT], handle)
+        self.binSound = pygame.mixer.Sound(os.path.join('assets', 'sounds', 'bin.wav'))
+        self.hackSound = pygame.mixer.Sound(os.path.join('assets', 'sounds', 'hack.wav'))
+        self.leafSound = pygame.mixer.Sound(os.path.join('assets', 'sounds', 'leaves2.wav'))
+        self.shootSound = pygame.mixer.Sound(os.path.join('assets', 'sounds', 'shoot.wav'))
 
-    def process(self, entities, dt):
-    	pass
+    def process(self, event):
+        if event.code == "bin":
+            self.binSound.play()
+        elif event.code == 'terminal':
+            self.hackSound.play()
+        elif event.code == 'plant':
+            self.leafSound.play()
+        elif event.code == 'shoot':
+            self.shootSound.play()
+        else:
+            print "Event code not known!"
 
     def onAttach(self, world):
         super(SoundSystem, self).onAttach(world)
         def handle(event):
             self.eventQueue.append(event)
-        world.on([pygame.USEREVENT], handle)
